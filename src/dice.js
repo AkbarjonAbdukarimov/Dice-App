156d35e7cf1cab49483b25ab9c00304e1ccdd860
AkbarjonAbdukarimov <100358259+AkbarjonAbdukarimov@users.noreply.github.com>
2022-07-20 17:33:16 +0500

    add die compopnent and start workingh on rolling logic

---
+++ src/dice.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+class Dice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { die1: "one", die2: "one" };
+  }
+  render() {}
+}
